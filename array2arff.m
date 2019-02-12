725adcf9871b792fbbf241184c56820195f8cd19
Fabio Ortega <fabiojose.muneratti@upf.edu>
2019-02-12 12:27:23 +0100

    Fixed bugs in interval calculation and added utility function for creating any arff from matlab array.

---
+++ array2arff.m
@@ -0,0 +1,36 @@
+% Writes an ARFF file with data from a matlab array.
+% Parameters:
+% . x: array with one sample per row and one attribute per column
+% . filename: char array with valid name or path+name for saving the file
+% . attrnames: cell array of char arrays with size(attrnames,1) = size(x,2)
+% holding attribute names.
+% . attrtypes: cell array of same structure as attrnames, for attribute
+% types.
+% . attrformat: either char array with full printf format (no \n) of
+% attribute values, or cell array in the same structure as the others with
+% one string format per attribute (in which case formats will be 
+% comma-separated).
+function array2arff(x, filename, attrnames, attrtypes, attrformat)
+
+if iscell(attrformat)
+    fmt = [];
+    for k = 1:size(attrformat,1)
+        fmt = [fmt attrformat{k} ',']; %#ok<AGROW>
+    end
+    attrformat = fmt(1:end-1);
+end
+
+F = fopen(filename, 'wt');
+fprintf(F, '%s', ['@relation ' filename newline newline]);
+for i = 1:size(x,2)
+fprintf(F, '%s', ['@attribute ' attrnames{i} ' ' attrtypes{i} newline]);
+end
+fprintf(F, '%s', [newline '@data' newline]);
+
+for i = 1:size(x,1)
+  fprintf(F,[attrformat '\n'],x(i,:));
+end
+
+fclose(F);
+
+end
--- score2arff.m
+++ score2arff.m
@@ -3,6 +3,6 @@ function score2arff (score_filename, arff_filename)
 addpath(genpath('musicXML'));
 
 sc = parseMusicXML(score_filename);
-makeArff(arff_filename, sc);
-
+[f, info] = noteFeatures(sc);
+array2arff(f, arff_filename, info(:,1), info(:,2), info(:,3));
 end
